992321bc8a4e8dfe82a3826f2a1c50d81d4c383b
alicevia <ed-phoenix@qq.com>
2022-09-21 18:39:19 +0800

    'temp'

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,4 +1,14 @@
+import { TOKEN } from '@/constants'
 import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+
+function useToken () {
+  const token = useStorage(TOKEN, '')
+
+  return {
+    token
+  }
+}
 
 export const useUserStore = defineStore('user', () => {
   const token = useStorage<string>('token', '')
